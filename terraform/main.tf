c41389653a0aaa7b6f23ab53bb8e42763b4732f1
omarchouki <omarchoki93@gmail.com>
2024-08-11 00:49:27 +0100

    teste pour dataset

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "google" {
+  credentials = file("C:/Users/Chouki_Omar/Desktop/data_project/eternal-splicer-432020-h2-7220d97f933d.json")
+  project     = "eternal-splicer-432020-h2"
+  region      = "us-central1"
+}
+
+resource "google_bigquery_dataset" "test_dataset" {
+  dataset_id                  = "test_dataset"
+  location                    = "US"
+  description                 = "A new description"
+  default_table_expiration_ms = 3600000 # 1 hour
+}
+
+
+output "dataset_id" {
+  value = google_bigquery_dataset.test_dataset.dataset_id
+}
