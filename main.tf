fa114077f9aec80f902647790c179c5d7211fb5f
omarchouki <omarchoki93@gmail.com>
2024-08-16 22:53:28 +0100

    creation tablexds bucket vk

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("https://drive.google.com/file/d/1uJoM3qTDcmRIeFcQXMBS3rGzh8xb0bfJ/view?usp=sharing")
+  credentials = file("C:/Users/Chouki_Omar/Downloads/eternal-splicer-432020-h2-7220d97f933d.json")
   project     = "eternal-splicer-432020-h2"
   region      = "us-central1"
 }
