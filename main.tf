13cadd39adb31f932774be0d277fc987c8372ec4
omarchouki <omarchoki93@gmail.com>
2024-08-16 23:15:51 +0100

    table 2

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_bigquery_dataset" "test_dataset" {
-  dataset_id                  = "test_dataset"
+  dataset_id                  = "test_dataset_"
   location                    = "US"
   description                 = "A new description"
   default_table_expiration_ms = 3600000 # 1 hour
@@ -17,6 +17,32 @@ output "dataset_id" {
 }
 
 resource "google_bigquery_table" "test_table" {
+  dataset_id = google_bigquery_dataset.test_dataset.dataset_id
+  table_id   = "test_table"
+  project    = "eternal-splicer-432020-h2"
+
+  schema = jsonencode([
+    {
+      "name" = "id"
+      "type" = "INTEGER"
+      "mode" = "REQUIRED"
+    },
+    {
+      "name" = "name"
+      "type" = "STRING"
+      "mode" = "NULLABLE"
+    },
+    {
+      "name" = "created_at"
+      "type" = "TIMESTAMP"
+      "mode" = "NULLABLE"
+    }
+  ])
+
+  # Vous pouvez ajouter des données de test directement à la création de la table avec des options comme 'external_data_configuration'
+}
+
+resource "google_bigquery_table" "test_table_2" {
   dataset_id = google_bigquery_dataset.test_dataset.dataset_id
   table_id   = "test_table"
   project    = "eternal-splicer-432020-h2"
